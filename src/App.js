2d00e6d6060f0a6a26aabeaa3ea2ffebacea795e
akam <albertkam91@gmail.com>
2017-08-30 17:22:57 -0700

    changed name of app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>"X" Sweeper</h2>
+        <h2>KamSweeper</h2>
         <Board/>
       </div>
     );
